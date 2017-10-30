2fb09f74b50c51b76b9f2cca55e05207e880e1be
Alexey Novakov <an@datawerks.com>
2017-10-30 22:08:13 +0100

    complete monoid

--- exercises/src/main/scala/fpinscala/monoids/Monoid.scala
+++ exercises/src/main/scala/fpinscala/monoids/Monoid.scala
@@ -165,14 +165,24 @@ object Monoid {
     override def zero: (A, B) = (A.zero, B.zero)
   }
 
-  def functionMonoid[A, B](B: Monoid[B]): Monoid[A => B] =
-    ???
+  def functionMonoid[A, B](B: Monoid[B]): Monoid[A => B] = new Monoid[A => B] {
+    override def op(a1: A => B, a2: A => B): A => B =
+      a => B.op(a1(a), a2(a))
+
+    override def zero: A => B = _ => B.zero
+  }
 
   def mapMergeMonoid[K, V](V: Monoid[V]): Monoid[Map[K, V]] =
-    ???
+    new Monoid[Map[K, V]] {
+      def zero = Map[K,V]()
+      def op(a: Map[K, V], b: Map[K, V]) =
+        (a.keySet ++ b.keySet).foldLeft(zero) { (acc,k) =>
+          acc.updated(k, V.op(a.getOrElse(k, V.zero),
+            b.getOrElse(k, V.zero)))
+        } }
 
   def bag[A](as: IndexedSeq[A]): Map[A, Int] =
-    ???
+    foldMapV(as, mapMergeMonoid[A, Int](intAddition))((a: A) => Map(a -> 1))
 }
 
 trait Foldable[F[_]] {
