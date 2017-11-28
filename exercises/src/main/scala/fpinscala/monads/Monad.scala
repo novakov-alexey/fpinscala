ef5d0294facc063994a1375763a73af60c521030
novakov_alex <novakov.alex@gmail.com>
2017-11-28 22:57:59 +0100

    save last progress on Monads

--- exercises/src/main/scala/fpinscala/monads/Monad.scala
+++ exercises/src/main/scala/fpinscala/monads/Monad.scala
@@ -31,16 +31,38 @@ trait Monad[M[_]] extends Functor[M] {
   def unit[A](a: => A): M[A]
   def flatMap[A,B](ma: M[A])(f: A => M[B]): M[B]
 
+  def product[A,B](ma: M[A], mb: M[B]): M[(A, B)] = map2(ma, mb)((_, _))
+
+  def filterM[A](ms: List[A])(f: A => M[Boolean]): M[List[A]] =
+    ms match {
+      case Nil => unit(Nil)
+      case h :: t => flatMap(f(h))(a => {
+        if (!a) filterM(t)(f)
+        else map(filterM(t)(f))(h :: _)
+      })
+    }
+
   def map[A,B](ma: M[A])(f: A => B): M[B] =
     flatMap(ma)(a => unit(f(a)))
+
   def map2[A,B,C](ma: M[A], mb: M[B])(f: (A, B) => C): M[C] =
     flatMap(ma)(a => map(mb)(b => f(a, b)))
 
-  def sequence[A](lma: List[M[A]]): M[List[A]] = ???
+  def sequence[A](lma: List[M[A]]): M[List[A]] =
+    lma match {
+      case Nil => unit(Nil)
+      case h :: t => map2(h, sequence(t))(_ :: _)
+    }
 
-  def traverse[A,B](la: List[A])(f: A => M[B]): M[List[B]] = ???
+  def traverse[A,B](la: List[A])(f: A => M[B]): M[List[B]] =
+    la match {
+      case Nil => unit(Nil)
+      case h::t => map2(f(h), traverse(t)(f))(_ :: _)
+    }
 
-  def replicateM[A](n: Int, ma: M[A]): M[List[A]] = ???
+  def replicateM[A](n: Int, ma: M[A]): M[List[A]] =
+    if (n <= 0) unit(List())
+    else map2(ma, replicateM(n - 1, ma))(_ :: _)
 
   def compose[A,B,C](f: A => M[B], g: B => M[C]): A => M[C] = ???
 
@@ -62,17 +84,42 @@ object Monad {
       ma flatMap f
   }
 
-  val parMonad: Monad[Par] = ???
+  val parMonad: Monad[Par] = new Monad[Par] {
+    override def flatMap[A, B](ma: Par[A])(f: A => Par[B]): Par[B] = ma.flatMap(f)
 
-  def parserMonad[P[+_]](p: Parsers[P]): Monad[P] = ???
+    override def unit[A](a: => A): Par[A] = Par.unit(a)
+  }
 
-  val optionMonad: Monad[Option] = ???
+  def parserMonad[P[+_]](p: Parsers[P]): Monad[P] = new Monad[P] {
+    override def flatMap[A, B](ma: P[A])(f: A => P[B]): P[B] = p.flatMap(ma)(f)
 
-  val streamMonad: Monad[Stream] = ???
+    override def unit[A](a: => A): P[A] = p.succeed(a)
+  }
 
-  val listMonad: Monad[List] = ???
+  val optionMonad: Monad[Option] = new Monad[Option] {
+    override def flatMap[A, B](ma: Option[A])(f: A => Option[B]): Option[B] = ma.flatMap(f)
 
-  def stateMonad[S] = ???
+    override def unit[A](a: => A): Option[A] = Option(a)
+  }
+
+  val streamMonad: Monad[Stream] = new Monad[Stream] {
+    override def flatMap[A, B](ma: Stream[A])(f: A => Stream[B]): Stream[B] = ma.flatMap(f)
+
+    override def unit[A](a: => A): Stream[A] = Stream(a)
+  }
+
+  val listMonad: Monad[List] = new Monad[List] {
+    override def flatMap[A, B](ma: List[A])(f: A => List[B]): List[B] = ma.flatMap(f)
+
+    override def unit[A](a: => A): List[A] = List(a)
+  }
+
+  def stateMonad[S]: Monad[({type f[x] = State[S, x]})#f] = new Monad[({type f[x] = State[S, x]})#f] {
+    def unit[A](a: => A): State[S, A] = State.unit(a)
+
+    override def flatMap[A,B](st: State[S, A])(f: A => State[S, B]): State[S, B] =
+      st flatMap f
+  }
 
   val idMonad: Monad[Id] = ???
 
