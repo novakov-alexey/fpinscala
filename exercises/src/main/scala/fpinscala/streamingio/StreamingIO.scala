6484aab67ce6c05fb6a2931d5fe9047843adf750
Alexey Novakov <an@datawerks.com>
2018-02-02 21:49:50 +0100

    save progress on StreamsIO

--- exercises/src/main/scala/fpinscala/streamingio/StreamingIO.scala
+++ exercises/src/main/scala/fpinscala/streamingio/StreamingIO.scala
@@ -292,13 +292,27 @@ object SimpleStreamTransducers {
     /*
      * Exercise 1: Implement `take`, `drop`, `takeWhile`, and `dropWhile`.
      */
-    def take[I](n: Int): Process[I,I] = ???
+    def take[I](n: Int): Process[I,I] = {
+      if (n <= 0) Halt()
+      else await(i => emit(i, take(n - 1)))
+    }
 
-    def drop[I](n: Int): Process[I,I] = ???
+    def drop[I](n: Int): Process[I,I] = {
+      if (n <= 0) id
+      else await(i => drop[I](n-1))
+    }
 
-    def takeWhile[I](f: I => Boolean): Process[I,I] = ???
+    def takeWhile[I](f: I => Boolean): Process[I,I] =
+      await(i =>
+        if (f(i)) emit(i, takeWhile(f))
+        else Halt()
+      )
 
-    def dropWhile[I](f: I => Boolean): Process[I,I] = ???
+    def dropWhile[I](f: I => Boolean): Process[I,I] =
+      await(i =>
+        if (f(i)) dropWhile(f)
+        else emit(i, id)
+      )
 
     /* The identity `Process`, just repeatedly echos its input. */
     def id[I]: Process[I,I] = lift(identity)
